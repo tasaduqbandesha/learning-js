111f2eee6ad6a9d8c444b86c36b3f09d2823c5e7
Tasaduq <tasaduq@renesistech.com>
2023-02-02 11:32:38 +0500

    lab1

---
+++ lab1.js
@@ -0,0 +1,68 @@
+<script>
+{/* Q1:Is JS client side or server side language?
+ans: Js is a client side language 
+
+Q2:Does JS produces Source/Object code?
+No,javaScript code is executed directly by the web browser 
+
+Q3: List Down All Data types?
+ 1:Primitive 
+ Bolean
+ Null
+ Number
+ String
+ Undefine
+ Symbol
+
+ 2:NonPrimitive
+ Array
+ Object
+
+ Q4: In how many ways a variable can be declared?
+ ans :
+  ways:  var,  let, const ,nothing 
+
+ Q5: What does a variable scope mean?
+ ans: scope of a variable refers to the region of the code
+   two types in javaScript,
+
+   1:Global scope
+   
+   Global scope is declared out side of any function 
+
+   2:Local scope
+
+   Global scope is declared in side of any function 
+
+
+Q6: code 
+name new name
+inside if new name
+its new name new name
+   
+   Q7: code
+
+ name new name
+ inside if new name
+Error message Not define newName
+     
+ Q8: code
+
+name new name 
+inside if new name
+Error message Not define newName
+
+    
+
+ */}
+
+
+
+
+
+
+
+
+
+
+</script>
